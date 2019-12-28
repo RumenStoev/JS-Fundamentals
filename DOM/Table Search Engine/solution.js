af0ece988fc511fb1cf4d2c3f888fb738600f662
RumenS <49361738+RumenStoev@users.noreply.github.com>
2019-12-28 22:15:54 +0200

    Create solution.js

---
+++ DOM/Table Search Engine/solution.js
@@ -0,0 +1,19 @@
+function solve() {
+    let button = document.getElementById("searchBtn");
+    let input = document.getElementById("searchField")
+
+
+    button.addEventListener("click", search)
+
+    function search() {
+        let elementTR = Array.from(document.querySelectorAll(" tbody > tr >td "))
+        elementTR.map((x) => {
+            if(x.innerHTML === input.value) {
+                x.parentElement.className = "select"
+            }
+        })
+    }
+}
+
+
+
