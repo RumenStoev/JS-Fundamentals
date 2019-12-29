a1c37846092eaf5fb97ad768f2e7f2debc473246
RumenS <49361738+RumenStoev@users.noreply.github.com>
2019-12-29 18:02:19 +0200

    Update app.js

--- DOM/Delete From Table/app.js
+++ DOM/Delete From Table/app.js
@@ -23,3 +23,31 @@ function solve() {
 
 
 document.addEventListener("DOMContentLoaded",solve)
+
+
+
+
+
+
+//Another solution
+
+function deleteElement(input) {
+    let elementItems = Array.from(document.querySelectorAll("#customers > tbody > tr > td"))
+        elementItems.map(x =>{
+            if(x.innerHTML === input.value) {
+                x.parentNode.remove()
+            }})
+
+}
+
+function solve() {
+    let button  = document.getElementById("btn")
+   let input = document.querySelector("body > label > input[type=text]");
+    if(button === null || input === null ) {
+        throw new Error("Have a some Error!!!")
+    }
+    button.addEventListener("click",deleteElement.bind(undefined,input))
+}
+
+
+
