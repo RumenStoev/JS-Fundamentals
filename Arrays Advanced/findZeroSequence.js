ccfcefcef3ed3a0d65f623f4aab3520e63dc4967
RumenS <49361738+RumenStoev@users.noreply.github.com>
2023-03-14 14:06:33 +0200

    Update findZeroSequence.js

--- Arrays Advanced/findZeroSequence.js
+++ Arrays Advanced/findZeroSequence.js
@@ -1,24 +1,22 @@
 function findZeroSequence(arr) {
-    let result = [];
+    let result = {}
     let containerZero = [];
+
 
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === 0) {
             containerZero.push(i)
-            result[containerZero[0]] = [containerZero[0], containerZero.length]
+            result[containerZero[0]] = containerZero.length
         } else {
             containerZero = [];
+
         }
     }
 
-    return result
-        .map((item) => {
-            if (Array.isArray(item)) {
-                return `Zero start from index ${item[0]} sequences ${item[1]}`;
-            }
-        })
-        .filter(Boolean)
-        .join("\n");
+    return Object.entries(result).map(([prop, value], key) => {
+        return `Start index -> ${prop} sequences ${value}`
+    }).join("\n")
+
 }
 
 
@@ -27,5 +25,5 @@ console.log(
     findZeroSequence([1, 1, 1, 0, 0, 0, 1, 1, 1, 0])
 )
 
-->  Zero start from index 3 sequences 3
-    Zero start from index 9 sequences 1
+->  Start index -> 3 sequences 3
+    Start index -> 9 sequences 1
